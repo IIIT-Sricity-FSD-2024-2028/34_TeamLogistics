82da7b1a9808025dd09f093180f17ac628c3e7a8
Granthik6 <granthikranadheer.t24@iiits.in>
2026-03-09 12:59:09 +0530

    Update DBschema.sql

--- DATABASE/DBschema.sql
+++ DATABASE/DBschema.sql
@@ -1,11 +1,16 @@
+-- create a database 
 CREATE DATABASE DeliverSync;
+-- use the database 
 USE DeliverSync;
+-- create the tables
+-- create the role table
 CREATE TABLE Role (
     role_id INT AUTO_INCREMENT PRIMARY KEY,
     role_name VARCHAR(50) NOT NULL UNIQUE
 );
+-- create the Users table   
 CREATE TABLE Users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT AUTO_INCREMENT PRIMARY KEY, 
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     phone VARCHAR(15) NOT NULL,
@@ -13,15 +18,28 @@ CREATE TABLE Users (
     role_id INT NOT NULL,
     FOREIGN KEY (role_id) REFERENCES Role(role_id)
 );
+-- create the fleet Manager table
+CREATE TABLE Fleet_Manager (
+    fleet_manager_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    company_name VARCHAR(120) NOT NULL,
+    company_address TEXT,
+    number_of_vehicles INT CHECK (number_of_vehicles >= 0),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
+);
+-- create the Vehicle table
 CREATE TABLE Vehicle (
     vehicle_id INT AUTO_INCREMENT PRIMARY KEY,
+    fleet_manager_id INT,
     vehicle_number VARCHAR(30) UNIQUE NOT NULL,
     vehicle_company VARCHAR(100),
     model VARCHAR(100),
     year INT CHECK (year >= 2000),
     color VARCHAR(50),
-    type VARCHAR(50)
+    type VARCHAR(50),
+    FOREIGN KEY (fleet_manager_id) REFERENCES Fleet_Manager(fleet_manager_id) ON DELETE CASCADE
 );
+-- create the Driver table
 CREATE TABLE Driver (
     driver_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
@@ -30,6 +48,7 @@ CREATE TABLE Driver (
     status ENUM('Active','Inactive','Suspended') DEFAULT 'Active',
     FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
 );
+-- create the Business_client table
 CREATE TABLE Business_Client (
     client_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
@@ -38,6 +57,7 @@ CREATE TABLE Business_Client (
     company_contact_num VARCHAR(15) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
 );
+-- create the Delivery_Request table
 CREATE TABLE Delivery_Request (
     request_id INT AUTO_INCREMENT PRIMARY KEY,
     client_id INT NOT NULL,
@@ -48,16 +68,20 @@ CREATE TABLE Delivery_Request (
     request_status ENUM('Pending','Assigned','Completed','Cancelled') DEFAULT 'Pending',
     FOREIGN KEY (client_id) REFERENCES Business_Client(client_id) ON DELETE CASCADE
 );
+-- create the Assignment table
 CREATE TABLE Assignment (
     assignment_id INT AUTO_INCREMENT PRIMARY KEY,
+    fleet_manager_id INT,
     request_id INT NOT NULL,
     driver_id INT,
     vehicle_id INT,
     assignment_status ENUM('Assigned','In_Progress','Completed','Cancelled') DEFAULT 'Assigned',
     FOREIGN KEY (request_id) REFERENCES Delivery_Request(request_id) ON DELETE CASCADE,
     FOREIGN KEY (driver_id) REFERENCES Driver(driver_id) ON DELETE SET NULL,
-    FOREIGN KEY (vehicle_id) REFERENCES Vehicle(vehicle_id) ON DELETE SET NULL
+    FOREIGN KEY (vehicle_id) REFERENCES Vehicle(vehicle_id) ON DELETE SET NULL,
+    FOREIGN KEY (fleet_manager_id) REFERENCES Fleet_Manager(fleet_manager_id) ON DELETE SET NULL
 );
+-- create the Trip table
 CREATE TABLE Trip (
     trip_id INT AUTO_INCREMENT PRIMARY KEY,
     assignment_id INT NOT NULL, 
@@ -67,6 +91,7 @@ CREATE TABLE Trip (
     trip_status ENUM('Started','In_Transit','Delivered','Cancelled') DEFAULT 'Started',
     FOREIGN KEY (assignment_id) REFERENCES Assignment(assignment_id) ON DELETE CASCADE
 );
+-- create the proof_of_Delivery table
 CREATE TABLE Proof_of_Delivery (
     trip_id INT PRIMARY KEY,
     delivery_image TEXT,
@@ -74,6 +99,7 @@ CREATE TABLE Proof_of_Delivery (
     delivered_time TIMESTAMP,
     FOREIGN KEY (trip_id) REFERENCES Trip(trip_id) ON DELETE CASCADE
 );
+-- create the Invoice table
 CREATE TABLE Invoice (
     invoice_id INT AUTO_INCREMENT PRIMARY KEY,
     request_id INT NOT NULL,
@@ -82,6 +108,7 @@ CREATE TABLE Invoice (
     payment_status ENUM('Pending','Paid','Overdue') DEFAULT 'Pending',
     FOREIGN KEY (request_id) REFERENCES Delivery_Request(request_id) ON DELETE CASCADE
 );
+-- create the Document table
 CREATE TABLE Document (
     document_id INT AUTO_INCREMENT PRIMARY KEY,
     driver_id INT,
@@ -91,6 +118,7 @@ CREATE TABLE Document (
     FOREIGN KEY (driver_id) REFERENCES Driver(driver_id) ON DELETE SET NULL,
     FOREIGN KEY (vehicle_id) REFERENCES Vehicle(vehicle_id) ON DELETE SET NULL
 );
+-- create the Maintenance table
 CREATE TABLE Maintenance (
     maintenance_id INT AUTO_INCREMENT PRIMARY KEY,
     vehicle_id INT NOT NULL,
@@ -100,6 +128,7 @@ CREATE TABLE Maintenance (
     maintenance_status ENUM('Scheduled','In_Service','Completed') DEFAULT 'Scheduled',
     FOREIGN KEY (vehicle_id) REFERENCES Vehicle(vehicle_id) ON DELETE CASCADE
 );
+-- create the Compliance table
 CREATE TABLE Compliance (
     compliance_id INT AUTO_INCREMENT PRIMARY KEY,
     driver_id INT NOT NULL,
@@ -109,6 +138,7 @@ CREATE TABLE Compliance (
     compliance_status ENUM('Valid','Invalid') DEFAULT 'Valid',
     FOREIGN KEY (driver_id) REFERENCES Driver(driver_id) ON DELETE CASCADE
 );
+-- create the Notification table
 CREATE TABLE Notification (
     notification_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
